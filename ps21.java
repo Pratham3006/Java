a417b2c04be3fb7995529dc6ce1bc79e03f069a5
Pratham3006 <prathamshetty71@gmail.com>
2024-07-08 18:00:01 +0530

    Ternary operator

---
+++ ps21.java
@@ -0,0 +1,10 @@
+public class ps21 {
+    public static void main(String[] args) {
+        System.out.println(comp(25,20));
+
+    }
+    public static int comp(int a,int b){
+        int m=(a>b?a+b:a-b);
+        return m;
+    }
+}
