f02e98d262874f79be2904d161181036c8d1e48a
Pratham3006 <prathamshetty71@gmail.com>
2024-07-08 15:16:11 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	out/production/Practice/ps12.class
    #	out/production/Practice/ps13.class
    #	out/production/Practice/ps14.class
    #	out/production/Practice/ps15.class
    #	out/production/Practice/ps16.class

---
+++ ps17.java
@@ -0,0 +1,8 @@
+public class ps17 {
+    public static void main(String[] args) {
+        //Binary to hex//
+       String a="100";
+       int x=Integer.parseInt(a,2);
+        System.out.println(Integer.toHexString(x));
+    }
+}
