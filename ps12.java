f42da0456b984cadc9f6a237b7b18ec0d733389c
Pratham3006 <prathamshetty71@gmail.com>
2024-07-08 14:06:40 +0530

    Changes

--- ps12.java
+++ ps12.java
@@ -1,2 +1,9 @@
 public class ps12 {
+    public static void main(String[] args) {
+        System.out.println(conv("255"));
+    }
+    public static String conv(String a){
+        int x=Integer.parseInt(a);
+        return Integer.toHexString(x);
+    }
 }
